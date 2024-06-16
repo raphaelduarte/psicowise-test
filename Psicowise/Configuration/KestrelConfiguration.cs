2853a1aca94d9693d07d013748696f7bdf58ee72
raphaelreshut <raphael@reshuttech.com>
2024-06-16 01:07:17 -0300

    Add project files.

---
+++ Psicowise/Configuration/KestrelConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using Psicowise.Configuration.Helpers;
+using static Psicowise.Configuration.Helpers.KestrelHelper;
+
+namespace Psicowise.Configuration
+{
+    public class KestrelConfiguration : KestrelHelper
+    {
+        public Endpoints Endpoints { get; set; }
+    }
+}
