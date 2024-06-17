e4bef3f85f1dc1bdab082edcf551db26e6d748ea
raphaelreshut <raphael@reshuttech.com>
2024-06-16 21:58:39 -0300

    arrumando a senha do aspnetapp.pfx

--- Psicowise/Program.cs
+++ Psicowise/Program.cs
@@ -15,7 +15,7 @@ builder.WebHost.ConfigureKestrel(serverOptions =>
     serverOptions.Listen(IPAddress.Any, 80);
     serverOptions.Listen(IPAddress.Any, 443, listenOptions =>
     {
-        listenOptions.UseHttps("/app/certs/https/aspnetapp.pfx", "Joaquim12@");
+        listenOptions.UseHttps("/app/certs/https/aspnetapp.pfx", "1234567890");
     });
 });
 
